7f1f919341f7a3fea2a8212e2fa1260be9d373d1
서상원 <kiaofk@nm-fc.com>
2022-07-12 15:20:52 +0900

    child test

---
+++ src/main/java/com/example/subtreechild/SubtreeChildApplication.java
@@ -0,0 +1,13 @@
+package com.example.subtreechild;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubtreeChildApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubtreeChildApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/subtreechild/SubtreeChildApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.subtreechild;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubtreeChildApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
